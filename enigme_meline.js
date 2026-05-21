40e7930c94769ac7befb53531b58413a25ff77cc
Alexandreprojetspersos <enigme.meline@gmail.com>
2026-05-22 01:03:32 +0200

    Add files via upload

--- enigme_meline.js
+++ enigme_meline.js
@@ -1,46 +1,57 @@
 const enigme1 = `
 	<h2> Bravo, du as réussis la première enigme <h2>
 	<p> Le premier code est donc : 
-	
-	La première anecdote dont j'ai envie de te faire part est une histoire assez drôle. Je ne sais pas si tu te souviens mais l'année dernière pour la fin de l'année Monsieur Daoudal t'avais fais donner des bonbons secs. 
-	J'ai gardé celui que tu m'avais donné et je l'ai emmené comme porte bonheur à chacune des épreuves du bac (il a l'air plutôt efficace pour le moment).</p>` ;
+	Anecdote : L'année dernière, lorsque je suis arrivé au lycée après avoir redoublé ma première, j'étais pas au top de ma forme. Je suis parti du principe que cette nouvelle année était une nouvelle chance et que je devais en profiter.
+	Je t'ai remarquée au bout d'environ deux ou trois semaines de cours, je ne sais plus exactement pourquoi (au début de l'année je te confondais toi et Lilou, ne me demande pas pourquoi, je n'en ai aucune idée). A partir de ce moment là,
+	je me suis rendu compte que si je voulais avoir une chance avec toi, je devais me reprendre en main. C'est donc grâce à toi que j'ai recommencé à travailler sérieusement, à essayer de communiquer davantage (j'étais un peu introverti),
+	à reprendre le piano et me mettre à la muscu. Je suis reconnaissant de la chance que j'ai eu de te croiser dans ma vie et je me suis rendu compte que tu es littéralement ma source de motivation et celle qui me permet de réaliser mes
+	objectifs (ce projet en est la preuve), c'est pourquoi j'ai envie que tu continues à en faire partie, par choix, pas par pulssion. Au début de l'année, j'avais comme ambition d'obtenir un IUT, tu m'as permis d'envisager la prépa ou 
+	la double licence pour viser les grandes écoles (bien que ça se soit passé à ton insu) et j'espère que tu as pu te rendre compte de ta vraie valeur à mes yeux.</p>` ;
 	
 const enigme2 = `
 
 	<h2> Bien joué c'est la bonne réponse</h2>
-	<p> La seconde anecdote que je vais te raconter est un moment qui m'a marqué. En effet, j'ai toujours eu envie de passer davantage de temps avec toi pour apprendre à te connaître et j'avais pour projet de venir te parler
-	durant le voyage dans le Vercors. Bien que je n'ai finalement pas osé par peur de te déranger, je me rappel de cette soirée jeux de sociétés durant laquelle toi et les autres filles s'étaient enfermées dans la salle commune de
-	votre aile avec Felix l'Animateur pour apprendre une dans sur un Logobi. Bien qu'on est arrivé un peu tard avec Roman (c'était la dernière répétition), je me rappellerai toujours que lorsque l'on est entrés dans la pièce, tu étais 
-	en face de moi et que tu m'as fait signe de nous joindre à cette danse. C'était vraiment très sympa de ta part. </p>` ;
+	<p> Anecdote : Quand je suis en ta présence, je tiens tellement à toi que j'évite systématiquement de faire un faux pas. Dans ma tête je suis toujours en train de chercher les comportements que tu pourrrais ne pas trouver acceptables
+	ou quoi dire pour engager une conversation sans paraître lourd, vulgaire ou simplet. C'est extrêmement difficile et je sais que mes émotions transparaissent facilement quand tu es en face de moi (tu es la seule qui me provoque une 
+	montée de ma fréquence cardiaque), c'est pour cette raison que je fuis tes yeux du regard. Pourtant, j'aimerais que tu saches que je trouve leur couleur ambrée vraiment magnifique et leur forme en amande donne une impression de gentillesse 
+	et de douceur instantanément. Je trouve ta manière de communiquer vraiment bienveillante car je ne t'ai jamais vu rejeter quelqu'un cependant j'ai l'impression que tu t'en sers un peu comme protection pour ne pas attrister les autres et 
+	éviter le conflit ce qui manque un peu de franchise de tant en tant selon moi.</p>` ;
 	
 const enigme3 = `
 
-<p> La troisième anecdote porte sur un sujet plus sérieux. En effet, lorsque je suis arrivé à Sainte Thérèse l'année dernière, je venais de redoubler et j'étais vraiment au plus bas. J'avais perdu toute confiance en moi mais je voyais en 
-même temps ce moment comme une chance de repartir sur de nouvelles bases dans un nouvel environnement. J'avoue que lorsque je t'ai remarquée au bout d'environ 1 mois de cours, tu as été l'élément déclencheur qui m'a fait prendre conscience 
-que je devais m'investir et me dépasser si je voulais ne serais ce que pouvoir faire jeu égal avec toi être un prétendant possible. C'est grâce à cet amour que je me suis dépassé sur de nombreux domaines et je t'en suis vraiment reconnaissant bien
-que ça se soit passé à ton insu. </p>`;
+<p> Anecdote : Je penses que tu ne te souviens pas du dernier jour de lycée de l'année dernière mais lors de ce dernier, Mr Daoudal t'avait demandé de distribuer des sortes de bonbons secs. C'est toi qui m'avait donné le paquet et étant 
+donné que c'est le seul souvenir que j'ai de toi, je l'ai conservé depuis ce jour. Je l'ai d'ailleurs emporter avec moi à toutes les épreuves du bac que j'ai passé (bac de frannçais) comme un porte bonheur. Il marche d'ailleurs plutôt bien !</p>`;
 
 const enigme4 = `
 
-<p> Ma quatrième anecdote t'es directement liée. En effet, je suppose que tu as remarqué les efforts que j'ai fait de temps en temps pour me rapprocher de toi en venant te poser des questions sur les concepts des maths expertes. Bon j'imagine 
-que l'approche était cramée à des kilomètres mais c'était la seule manière pour moi de pouvoir tenter d'engager le contact. Si lors de ces dicussions, je semblais un peu destabilisé, agité ou je ne te regardais pas dans les yeux, ce n'est pas 
-parce que tu n'avais pas d'intéret à mes yeux </p>` ;
+<p> </p>` ;
+
+const enigme4bis = `
+
+<p> </p>` ;
 
 const enigme5 = `
 
-<p>La cinquième anecdote est un besoin personnel de terminer quelque chose que j'avais commencé mais que je n'avais pas été en mesure de terminer. La fois où j'étais venu te demander à quelle heure tu finissais les cours et qu'Alicia avait 
-interrompu la converation, je voulais te demander si ça te disais qu'on aille parler et manger quelque chose dans quimper. Je voulais que tu saches que la proposition est toujours d'actualité et je serais vraiment super heureux de partager 
-ce moment avec toi. </p>` ;
+<p>Anecdote : l'année dernière c'est grâce à toi que j'ai repris le piano. En effet, j'avais comme projet de t'inviter à aller manger quelque part pour la Saint Valentin et je voulais essayer de trouver un endroit avec un piano pour
+te jouer un morceau. Finallement, je ne me sentais pas au niveau donc je n'ai jamais osé t'en faire la demande mais j'ai tout de même appris ce morceau de Bruno Mars et Lady Gaga qui me fait penser à toi. Je suis très heureux de pouvoir te partager 
+ma passion pour la musique, que j'ai depuis l'enfance. D'ailleurs n'hésite pas à me faire part de tes gouts musicaux et j'essaierait d'apprendre certains de tes morceux préférés. D'ailleurs si ça te dis de faire une sortie ensemble tous
+les deux quelques part, peut importe l'activité : cinéma, sport... sache que je suis partant car j'ai vraiment envie de te connaître davantage.</p>` ;
 
 const enigme6 = `
 	
-	<p> Je vais ici expliquer ma démarche artistique lorsque j'ai créé ces dessins que je voulais t'offrir. En effet, je n'est pas choisi le handball au hasard. Bien que nous ne nous soyons pas énormément parlé au cours de ces deux années
-	au lycée, j'ai pu constater ta passion pour le sport et en particulier le Handball. Par curiosité (et en temps que sportif/compétiteur), je suis allé voir les performances que tu as pu réaliser dans ton club (j'ai pas mal de potes qui 
-	ont été joueurs à Handball forestois donc je connaissais un peu le club déjà, j'ai même failli m'y inscrire avant de faire du tennis). J'ai alors découvert que tu étais très impliquée dans ton club et les regards sur les photos m'ont 
-	montré une fille détermoinée et forte, capable d'être hyperconcentrée et combative. C'était un côté que j'vais déjà remarqué chez toi mais c'était encore plus visible dans cet environnement et c'est quelque chose qui m'impressionne énormément.
+	<p> Anecdote : Le trait de carctère que je trouve le plus attirant chez toi est ta passion pour le sport (surtout pour le handball de ce que j'ai compris). J'ai voulu en savoir un peu plus sur ce sport bien que j'ai pas ma d'amis qui ont fait
+	du handball à la forêt-fouesnant (j'vais failli m'inscrire il y a trois quatre ans mais le tennis l'avait finallement emporté) et j'ai ainsi découvert une facette de toi que je n'avais pas décelé aux premiers abords : celle d'une  fille impliquée,
+	faisant preuve d'une énorme force de caractère, d'une détermination qui se lit dans ses yeux et qui semble s'épanouir dans son environnement. C'est exactement ce que je recherche chez une personne : quelqu'un de passionné et d'ouvert car 
+	c'est toujours avec ce type de personne que l'on apprend le mieux, que les relations sont saines et les conversations intéressantes. </p>
 	`; 
-	
+
 const enigme7 = `
+
+<p> Anecdote : Le moment que je conserve de ces deux ans côte à côte et pourtant si éloignés, c'est bien-sûr notre séjour dans le Vercors. Vous étiez dans une salle commune avec Felix l'animateur et vous répétiez une danse toutes ensembles. 
+Avec Roman, on est venu voir par hasard ce qu'il se passait de votre côté et on est arrivé pile lorsque vous alliez la reproduire pour la dernière fois de la soirée. Quand j'ai ouvert la porte, c'est toi la première personne que j'ai vu car
+ty te trouvait juste devant. Le fait que tu nous fasses signe de venir vous rejoindre était vraiment super sympa et bien que ma danse fut quelque peu rigide et mal à l'aise, j'ai pris énormément de plaisir à la faire à tes côtés.</p> `; 
+	
+const enigme8 = `
 
 <p> Anecdote : Je me suis pris à la dernière minute pour préparer ce cadeau donc j'ai mis uniquement une semaine pour faire ces quatres dessins (en partant de 0 expérience mais ce fut vraiment chouette, j'ai adoré apprendre à dessiner et je te 
 recommande vraiement de t'y mettre si c'est quelque chose qui t'attires. Le site web était une idée pour marquer le coup et faire quelque chose d'original. Je l'ai créé sur seulement 2 jours et je suis très fier de ce que j'ai réussi à accomplir. 
@@ -54,7 +65,7 @@ const zone = document.getElementById('zone-anecdote-1');
     if (r === "boussole") { 
         zone.innerHTML = enigme1;
     } else {
-        alert("Ce n'est pas ça...");
+        alert("Ce n'est pas la bonne réponse, essaies à nouveau !");
     }
 }
 
